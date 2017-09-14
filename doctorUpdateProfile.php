813eb753c3998246c7030e720ad935f1edabd6a4
Tushar Udayan Tiwari <tiwaritusharudayan@gmail.com>
2017-09-14 22:56:59 +0530

    Major changes done to the doctor's update page, and its verification page as well

--- doctorUpdateProfile.php
+++ doctorUpdateProfile.php
@@ -27,17 +27,29 @@
 				}
 				else {
 					$row = $result->fetch_assoc();
+					$fname = $row['FirstName'];
+					$mname = $row['MiddleName'];
+					$lname = $row['LastName'];
+					$sex = $row['Sex'];
+					$addr = $row['Address'];
+					$joinDate = $row['DateOfJoining'];
+					$spec = $row['Specialization'];
+					$qual = $row['Qualification'];
+					$nat = $row['Nationality'];
+					$dept = $row['Department'];
+					$country = $row['CountryCode']; $contact=$row['ContactNumber'];
+					$email = $row['Email'];
 					echo "<table>";
-					echo "<tr><td>Name : </td><td>".$row['FirstName'].' '.$row['LastName']."</td><td><a href='verifyDoctorProfileUpdate.php?var=name&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Sex : </td><td>".$row['Sex']."</td><td><a href='verifyDoctorProfileUpdate.php?var=sex&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Address : </td><td>".$row['Address']."</td><td><a href='verifyDoctorProfileUpdate.php?var=addr&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Joining Date : </td><td>".$row['DateOfJoining']."</td><td><a href='verifyDoctorProfileUpdate.php?var=date&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Specialization : </td><td>".$row['Specialization']."</td><td><a href='verifyDoctorProfileUpdate.php?var=spec&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Qualification : </td><td>".$row['Qualification']."</td><td><a href='verifyDoctorProfileUpdate.php?var=qual&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Nationality : </td><td>".$row['Nationality']."</td><td><a href='verifyDoctorProfileUpdate.php?var=nat&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Department : </td><td>".$row['Department']."</td><td><a href='verifyDoctorProfileUpdate.php?var=dept&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Contact Number : </td><td>".$row['countryCode'].$row['contactNumber']."</td><td><a href='verifyDoctorProfileUpdate.php?var=cont&updated=f&user=$user'><button>Update</button></a></td></tr>";
-					echo "<tr><td>Email : </td><td>".$row['Email']."</td><td><a href='verifyDoctorProfileUpdate.php?var=email&updated=f&user=$user'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Name : </td><td>".$row['FirstName'].' '.$row['LastName']."</td><td><a href='verifyDoctorProfileUpdate.php?var=name&updated=f&user=$user&val1=$fname&val2=$mname&val3=$lname'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Sex : </td><td>".$row['Sex']."</td><td><a href='verifyDoctorProfileUpdate.php?var=sex&updated=f&user=$user&val1=$sex&val2=n&val3=n'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Address : </td><td>".$row['Address']."</td><td><a href='verifyDoctorProfileUpdate.php?var=addr&updated=f&user=$user&val1=$addr&val2=n&val3=n'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Joining Date : </td><td>".$row['DateOfJoining']."</td><td><a href='verifyDoctorProfileUpdate.php?var=date&updated=f&user=$user&val1=$joinDate&val2=n&val3=n'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Specialization : </td><td>".$row['Specialization']."</td><td><a href='verifyDoctorProfileUpdate.php?var=spec&updated=f&user=$user&val1=$spec&val2=n&val3=n'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Qualification : </td><td>".$row['Qualification']."</td><td><a href='verifyDoctorProfileUpdate.php?var=qual&updated=f&user=$user&val1=$qual&val2=n&val3=n'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Nationality : </td><td>".$row['Nationality']."</td><td><a href='verifyDoctorProfileUpdate.php?var=nat&updated=f&user=$user&val1=$nat&val2=n&val3=n'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Department : </td><td>".$row['Department']."</td><td><a href='verifyDoctorProfileUpdate.php?var=dept&updated=f&user=$user&val1=$dept&val2=n&val3=n'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Contact Number : </td><td>".$row['CountryCode'].$row['ContactNumber']."</td><td><a href='verifyDoctorProfileUpdate.php?var=cont&updated=f&user=$user&val1=$country&val2=$contact&val3=n'><button>Update</button></a></td></tr>";
+					echo "<tr><td>Email : </td><td>".$row['Email']."</td><td><a href='verifyDoctorProfileUpdate.php?var=email&updated=f&user=$user&val1=$email&val2=n&val3=n'><button>Update</button></a></td></tr>";
 					echo "</table>";
 					echo "<table><tr><td><a href='doctorHome.php?user=$user'><button>Home</button></a></td></tr></table>";
 				}
--- verifyDoctorProfileUpdate.php
+++ verifyDoctorProfileUpdate.php
@@ -15,6 +15,9 @@
 			$var = $_GET['var'];
 			$updated = $_GET['updated'];
 			$user = $_GET['user'];
+			$val1 = $_GET['val1'];
+			$val2 = $_GET['val2'];
+			$val3 = $_GET['val3'];
 			
 			$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
 			
@@ -24,140 +27,362 @@
 			else {
 				if($var == 'name'){
 					if($updated == 't'){
-						echo "<table>";
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							$connection->close();
+							echo "Updation Failed. Could Not Connect";
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET FirstName='$val1',MiddleName='$val2',LastName='$val3' WHERE ID in (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							
+							if(!$result){
+								$connection->close();
+								echo "Updation Failed. No result returned";
+							}
+							else {
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+								$connection->close();
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form method='GET' action='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'>";
+						echo "<tr><input type='text' name='val1' value='$val1' /></tr>";
+						echo "<tr><input type='text' name='val2' value='$val2' /></tr>";
+						echo "<tr><input type='text' name='val3' value='$val3' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'sex'){
 					if($updated == 't'){
-						echo "<table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
 						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						if(!$connection){
+							$connection->close();
+							echo "Cannot Connect";
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET Sex='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+								$connection->close();
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form action='verifyDoctorProfileUpdate.php' method='GET'>";
+						echo "<tr><input type='radio' name='val1' value='Male'> Male</input></tr>";
+						echo "<tr><input type='radio' name='val1' value='Female'> Female</input></tr>";
+						echo "<tr><input type='radio' name='val1' value='Unknown'> Somewhere in between</input></tr>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'addr'){
 					if($updated == 't'){
-						echo "<table>";
-						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							echo "Cannot Connect";
+							$connection->close();
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET Address='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+								$connection->close();
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form method='GET' action='verifyDoctorProfileUpdate.php'>";
+						echo "<tr><input type='textarea' name='val1' value='$val1' /></tr>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'date'){
 					if($updated == 't'){
-						echo "<table>";
-						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							echo "Cannot Connect";
+							$connection->close();
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET DateOfJoining='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+								$connection->close();
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form action='verifyDoctorProfileUpdate.php' method='GET'>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<tr><input type='text' name='val1' value='$val1' /></tr>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'spec'){
 					if($updated == 't'){
-						echo "<table>";
-						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							echo "Cannot Connect";
+							$connection->close();
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET Specialization='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								$connection->close();
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form method='GET' action='verifyDoctorProfileUpdate.php'>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<select name='val1'>";
+						echo "<option value='Spec1'>Specialization 1</option>";
+						echo "<option value='Spec2'>Specialization 2</option>";
+						echo "<option value='Spec3'>Specialization 3</option>";
+						echo "<option value='Spec4'>Specialization 4</option>";
+						echo "<option value='Spec5'>Specialization 5</option>";
+						echo "</select>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'qual'){
 					if($updated == 't'){
-						echo "<table>";
-						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							echo "Cannot Connect";
+							$connection->close();
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET Qualification='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								$connection->close();
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form method='GET' action='verifyDoctorProfileUpdate.php'>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<select name='val1'>";
+						echo "<option value='Qual1'>Qualification 1</option>";
+						echo "<option value='Qual2'>Qualification 2</option>";
+						echo "<option value='Qual3'>Qualification 3</option>";
+						echo "<option value='Qual4'>Qualification 4</option>";
+						echo "<option value='Qual5'>Qualification 5</option>";
+						echo "</select>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'nat'){
 					if($updated == 't'){
-						echo "<table>";
-						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							echo "Cannot Connect";
+							$connection->close();
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET Nationality='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								$connection->close();
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form method='GET' action='verifyDoctorProfileUpdate.php'>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<select name='val1'>";
+						echo "<option value='Nat1'>Nationality 1</option>";
+						echo "<option value='Nat2'>Nationality 2</option>";
+						echo "<option value='Nat3'>Nationality 3</option>";
+						echo "<option value='Nat4'>Nationality 4</option>";
+						echo "<option value='Nat5'>Nationaloty 5</option>";
+						echo "</select>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'dept'){
 					if($updated == 't'){
-						echo "<table>";
-						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							echo "Cannot Connect";
+							$connection->close();
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET Department='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								$connection->close();
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form method='GET' action='verifyDoctorProfileUpdate.php'>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<select name='val1'>";
+						echo "<option value='Dept1'>Department 1</option>";
+						echo "<option value='Dept2'>Department 2</option>";
+						echo "<option value='Dept3'>Department 3</option>";
+						echo "<option value='Dept4'>Department 4</option>";
+						echo "<option value='Dept5'>Department 5</option>";
+						echo "</select>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'cont'){
 					if($updated == 't'){
-						echo "<table>";
-						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							echo "Cannot Connect";
+							$connection->close();
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET ContactNumber='$val2',CountryCode='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+								$connection->close();
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form method='GET' action='verifyDoctorProfileUpdate.php'>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<tr><input type='text' name='val1' value='$val1' /></tr>";
+						echo "<tr><input type='text' name='val2' value='$val2' /></tr>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
 				else if($var == 'email'){
 					if($updated == 't'){
-						echo "<table>";
-						
-						echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
-						echo "</table>";
+						$connection = new mysqli("localhost","root","tushar1997","HOSPMGMNT");
+						if(!$connection){
+							echo "Cannot Connect";
+							$connection->close();
+						}
+						else {
+							$result = $connection->query("UPDATE TEST_DOCTOR SET Email='$val1' WHERE ID IN (SELECT ID FROM TEST_DOCTOR_LOGIN WHERE Username='$user')");
+							if(!$result){
+								echo "Cannot Update";
+								$connection->close();
+							}
+							else {
+								echo "<table>";
+								echo "<tr><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+								echo "</table>";
+								$connection->close();
+							}
+						}
 					}
 					else {
 						echo "<table>";
-						
-						echo "<tr><td><a href='verifyDoctorProfileUpdate.php?user=$user&updated=t&var=$var'><button>Save</button></a></td><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
+						echo "<form method='GET' action='verifyDoctorProfileUpdate.php'>";
+						echo "<tr><input type='hidden' name='var' value='$var' /></tr>";
+						echo "<tr><input type='hidden' name='user' value='$user' /></tr>";
+						echo "<tr><input type='hidden' name='updated' value='t' /></tr>";
+						echo "<tr><input type='email' name='val1' value='$val1' /></tr>";
+						echo "<tr><td><input type='submit' value='Save' />";
+						echo "</td></form><td><a href='doctorUpdateProfile.php?user=$user'><button>Back</button></a></td></tr>";
 						echo "</table>";
 					}
 				}
